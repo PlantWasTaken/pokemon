18d4992e81cbf013f8103d533a19e45d7eefcee4
PlantWasTaken <69759382+PlantWasTaken@users.noreply.github.com>
2022-12-15 21:07:18 +0100

    Update exp.py

--- exp.py
+++ exp.py
@@ -1,7 +1,7 @@
 import pyautogui
 import time as t
 from PIL import ImageGrab
-
+import random
 
 t.sleep(1)
 xy_pos = (684, 387)
@@ -10,17 +10,46 @@ x2 = xy_pos[0]+2
 y1 = xy_pos[1]-2
 y2 = xy_pos[1]+2
 
-while(True):
-    igmr = ImageGrab.grab(bbox=(x1,y1,x2,y2))
-    pxl = igmr.load()
-    print(pxl[0,0][0])
-    if(pxl[0,0][0] > 160): #opekom appears
-        pyautogui.press('1') 
+##################
+xypos2 = (1139, 603)
+x11 = xypos2[0]-1
+x22 = xypos2[0]+1
+y11 = xypos2[1]-1
+y22 = xypos2[1]+1
 
-    else:
+
+while(True):
+    
+    igmr = ImageGrab.grab(bbox=(x1,y1,x2,y2))
+    igmr2 = ImageGrab.grab(bbox=(x11,y11,x22,y22))
+    pxl = igmr.load()
+    pxl2 = igmr2.load()
+    
+    if(pxl[1,0][0] > 160): #opekom appears
+        t.sleep(0.8)
+        if(pxl2[1,0] == (78, 78, 78)): #healing
+
+            pyautogui.press('1') 
+            t.sleep(0.1)
+            pyautogui.press('4') 
+
+            t.sleep(1.5) #text
+
+
+        else: #attack
+
+            pyautogui.press('1') 
+            t.sleep(0.1)
+            pyautogui.press('1') 
+
+
+            t.sleep(1.5) #text
+            
+    else: #move
         pyautogui.keyDown('a')
         t.sleep(0.75)
         pyautogui.keyUp('a')
         pyautogui.keyDown('d')
         t.sleep(0.75)
         pyautogui.keyUp('d')
+    
